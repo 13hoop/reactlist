f04d89a96c0c1e72fc38f7bf8e4ddc1ed9eb2878
YongRen <zhanxin13@gmail.com>
2017-05-12 17:02:27 +0800

    page show

--- reactdemo/src/index.js
+++ reactdemo/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  // <h1>Hello, world! This is a fucking React, baby!</h1>,
-  <p>i can chenge data automatically, coll!   hahh</p>,
-  // document.getElementById('root'),
- document.getElementsByClassName('content')[0]
-);
+function tick() {
+  const element = (
+    <div>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+      <p> ---------- first react page by 13Hoop</p>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementsByClassName('content')[0]
+  );
+}
+
+setInterval(tick, 1000);
