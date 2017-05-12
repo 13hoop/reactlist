bab0719f90b82ca37a862a32f15143bb6fbac1c8
YongRen <zhanxin13@gmail.com>
2017-05-12 15:28:43 +0800

    react init

---
+++ reactdemo/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  // <h1>Hello, world! This is a fucking React, baby!</h1>,
+  <p>i can chenge data automatically, coll!   hahh</p>,
+  // document.getElementById('root'),
+ document.getElementsByClassName('content')[0]
+);
