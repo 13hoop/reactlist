36d8eb9e3a8be493713ee219ba8126f6b816f0a5
YongRen <zhanxin13@gmail.com>
2017-05-15 16:33:48 +0800

    add todoList done

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -19,16 +19,18 @@ class App extends Component {
   render() {
     let todos = this.state.todoList.map((item, index) => {
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item}/>
         </li>
       )
     })
+    console.log(todos)
+
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo}/>
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)}/>
         </div>
         <ol>
           {todos}
@@ -36,9 +38,24 @@ class App extends Component {
       </div>
     )
   }
-  addTodo() {
-    console.log('here add todo ...')
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
   }
 }
 
 export default App;
+
+let id = 0
+function idMaker() {
+  id += 1
+  return id
+}
